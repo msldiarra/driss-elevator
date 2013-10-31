01d27122fbd63eedfb61c05f232e1f1972df4dbb
Miguel Basire <miguel.basire@gmail.com>
2013-11-01 00:11:14 +0100

    only a server at a time (port 88 for local tests)

--- subscribe.sh
+++ subscribe.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
 
-curl --request POST http://localhost:8080/resources/player/register?email=miguel.basire@gmail.com\&pseudo=Otis\&serverURL=http://localhost:8883
-curl --request POST http://localhost:8080/resources/player/register?email=miguel.basire1@gmail.com\&pseudo=UpAndDown\&serverURL=http://localhost:8882
-curl --request POST http://localhost:8080/resources/player/register?email=miguel.basire2@gmail.com\&pseudo=Omnibus\&serverURL=http://localhost:8881
+curl --request POST http://localhost:8080/resources/player/register?email=miguel.basire@gmail.com\&pseudo=Driss\&serverURL=http://localhost:8883
 
