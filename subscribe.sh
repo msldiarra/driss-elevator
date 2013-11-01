12b2b40c40262a15db220eb8f011dc01edfc9429
Miguel Basire <miguel.basire@gmail.com>
2013-11-01 01:00:05 +0100

    cloudbees deployement: package all depedencies in a single jar

--- subscribe.sh
+++ subscribe.sh
@@ -1,5 +1,7 @@
 #! /bin/sh
 
 
-curl --request POST http://localhost:8080/resources/player/register?email=miguel.basire@gmail.com\&pseudo=Driss\&serverURL=http://localhost:8883
+
+#curl --request POST http://localhost:8080/resources/player/register?email=miguel.basire@gmail.com\&pseudo=Driss\&serverURL=http://localhost:8883
+curl --request POST http://localhost:8080/resources/player/register?email=miguel.basire@gmail.com\&pseudo=Driss\&serverURL=http://code-elevator.miguelbasire.eu.cloudbees.net
 
