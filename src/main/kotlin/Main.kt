8f8d76075886085565df6199788129165ba55bbf
Miguel Basire <miguel.basire@gmail.com>
2013-11-03 19:59:52 +0100

    adding logs to main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,16 @@
 package fr.codestory.elevator
 
-
+import org.apache.log4j.Logger
 
 
 fun main( args : Array<String> ){
 
+    val logger = Logger.getLogger("MAIN")
+
+
     val port = Integer.parseInt(args.get(0))
+
+    logger?.info("Loading application on port "+port)
 
     val server  = CommandServer(port, DrissElevator())
     server.listenToElevatorEvents()
