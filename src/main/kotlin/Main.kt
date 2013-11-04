635730abc19d86100fac7903b95d046fce5a3f64
Miguel Basire <miguel.basire@gmail.com>
2013-11-04 09:22:17 +0100

    Algorithms could be chosen on start. Default is Driss

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,15 +3,41 @@ package fr.codestory.elevator
 import org.apache.log4j.Logger
 
 
-fun main( args : Array<String> ){
+enum class ElevatorAlgorithm {
+    UPANDDOWN
+    OMNIBUS
+    DRISS
+}
+
+
+fun newElevatorCommand(algo: ElevatorAlgorithm): ElevatorCommand {
+    return when(algo) {
+        ElevatorAlgorithm.UPANDDOWN -> {
+            UpAndDownElevator()
+        }
+        ElevatorAlgorithm.OMNIBUS -> {
+            OmnibusElevator()
+        }
+        ElevatorAlgorithm.DRISS -> {
+            DrissElevator()
+        }
+        else -> {
+            throw IllegalArgumentException("Unknown algorithm")
+        }
+    }
+}
+
+
+fun main(args: Array<String>) {
 
     val logger = Logger.getLogger("MAIN")
 
-
     val port = Integer.parseInt(args.get(0))
+    val algorithm = ElevatorAlgorithm.valueOf( if (args.size > 1)  args.get(1).toUpperCase() else "DRISS")
 
-    logger?.info("Loading application on port "+port)
+    logger?.info("Loading $algorithm algorithm on port $port")
 
-    val server  = CommandServer(port, DrissElevator())
+    val elevator = newElevatorCommand(algorithm)
+    val server = CommandServer(port, elevator)
     server.listenToElevatorEvents()
 }
